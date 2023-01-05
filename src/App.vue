639761005e97500544a220010fb2eb401dcc1711
Ertinox-08 <aymericdelange80@gmail.com>
2023-01-05 20:39:53 +0100

    Learning VueJS

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Lorem ipsum dolor sit amet.</h1>
+
+</template>
+
+<style>
+@import url("https://unpkg.com/reset-css@5.0.1/reset.css");
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap');
+
+html,
+body {
+  font-family: 'Poppins', sans-serif;
+  box-sizing: content-box;
+  font-size: 16px;
+}
+h1 {
+  font-size: 30px;
+}
+</style>
