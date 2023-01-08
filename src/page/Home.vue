88a9875f5b22b7988686d6228d3a8d4f81520e93
Ertinox-08 <aymericdelange80@gmail.com>
2023-01-08 18:22:35 +0100

    UberEats tutorial

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -5,6 +5,7 @@
 <script>
 // IMPORT
 import BDD from '../BDD';
+import {onMounted} from "vue";
 // COMPONENTS
 import RestaurantRow from "../components/RestaurantRow.vue"
 
@@ -14,8 +15,6 @@ export default {
     RestaurantRow
   },
   setup() {
-    console.log(BDD)
-
     class Restaurant {
       constructor(name, note, image, drive_time) {
         this.name = name
@@ -25,9 +24,25 @@ export default {
       }
     }
 
-    let resto = new Restaurant('Chez Nono', 4.9, 'img', '45mn')
-    console.log(resto)
-  }
+    let data_restaurant = []
+
+    const makeDataRestaurant = () => {
+      let three_restaurant = []
+
+      for (const restaurant of BDD) {
+        const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time)
+
+        if (three_restaurant.length === 2) {
+          three_restaurant.push(new_restaurant)
+          data_restaurant.push(three_restaurant)
+          three_restaurant = []
+        } else {
+          three_restaurant.push(new_restaurant)
+        }
+      }
+    }
+    onMounted(makeDataRestaurant)
+  },
 }
 </script>
 
