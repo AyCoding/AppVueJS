6e3c4a09cc67d6f15720bb199d1989b95c2b2182
Ertinox-08 <aymericdelange80@gmail.com>
2023-01-10 20:32:05 +0100

    UberEats tutorial

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="header">
       <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="">
-      <input type="text" placeholder="De quoi avez vous envie ?">
+      <input v-model="user_search_restaurant" type="text" placeholder="De quoi avez vous envie ?">
     </div>
     <div class="banner"></div>
     <RestaurantRow v-for="(data, index) in data_restaurant" :three_restaurant="data"/>
@@ -12,7 +12,7 @@
 <script>
 // IMPORT
 import BDD from '../BDD';
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 
 // COMPONENTS
 import RestaurantRow from "../components/RestaurantRow.vue"
@@ -33,12 +33,15 @@ export default {
     }
 
     let data_restaurant = ref([])
+    let all_restaurant = []
 
     const makeDataRestaurant = () => {
       let three_restaurant = []
 
       for (const restaurant of BDD) {
         const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time)
+        // make all restaurant array
+        all_restaurant.push(new_restaurant)
 
         if (three_restaurant.length === 2) {
           three_restaurant.push(new_restaurant)
@@ -49,11 +52,22 @@ export default {
         }
       }
     }
+    // User search restaurant
+    let user_search_restaurant = ref('')
+
+    watch(user_search_restaurant, new_value => {
+      let regex = RegExp(new_value)
+      let search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name))
+
+      console.log(search_restaurant)
+    })
+
     onMounted(makeDataRestaurant)
 
     // RETURN
     return {
-      data_restaurant
+      data_restaurant,
+      user_search_restaurant
     }
   },
 }
@@ -83,6 +97,7 @@ export default {
       text-indent: 20px;
     }
   }
+
   .banner {
     width: 100%;
     height: 200px;
